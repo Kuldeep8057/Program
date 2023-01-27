6393088f8fb0a1527577a85e7e7c8656e65ef0cc
KULDEEP GANGWAR <gangwarkuldeep27@gmail.com>
2023-01-27 10:17:12 +0530

    git push

--- C Program/class program/Array2D/sum_of_two_matrix.c
+++ C Program/class program/Array2D/sum_of_two_matrix.c
@@ -1,17 +1,60 @@
 #include<stdio.h>
 int main()
 {
-    int a[2][2],b[2][2],result[2][2],i,j;
-    printf("enter the of list matrix\n");
-    for(i=0;i<=2;i++)
-    {
-        for(j=0;j<=2;j++)
+    int A[10][10],B[10][10],sum[10][10],i,j,p,q,m,n;
+    printf("enter the row and col size of first matrix A\n");
+    scanf("%d%d",&m,&n);
+    printf("enter the row and col size of second matrix B\n");
+    scanf("%d%d",&p,&q);
+if (m==p && n==q)
         {
-            printf("enter a[%d][%d],i,j");
-            scanf("%f",&a[i][j])
+            printf("matrix can be added");
         }
-    }
-    printf("enter the 2nd matrix");
-    for(i=0;i<=)
+    else
+        {
+            printf("matrix can not be added");
+        }
+    printf("\nenter element of matrix of A");
+        for(i=0;i<=m;i++)
+            {
+            for(j=0;j<=n;j++)
+                {
+                    printf("enter a[%d][%d]=",m,n);
+                    scanf("%d",&A[i][j]);
+                }
+            }
+    printf("enter element of matrix of B\n");
+        for(i=0;i<=p;i++)
+            {
+            for(j=0;j<=q;j++)
+                {
+                    printf("enter a[%d][%d]=",p,q);
+                    scanf("%d",&B[i][j]);
+                }
+            } 
+        printf("\nMatrix A\n");     
+            for(i=0;i<=m;i++)
+                {printf("\n");
+                    for(j=0;j<=n;j++)
+                    printf("  %d",A[i][j]);
+                }    
+        printf("\nMatrix B\n");     
+            for(i=0;i<=p;i++)
+                    {printf("\n");
+                    for(j=0;j<=q;j++)
+                    printf("  %d",B[i][j]);
+                    }
+        for(i=0;i<=m;i++)
+                {for(j=0;j<=n;j++)
+                sum[i][j]=A[i][j]+B[i][j];
+                }
+            printf("\nMatrix Sum");
+            for(i=0;i<=m;i++)
+                {printf("\n");
+                    for(j=0;j<=n;j++)
+                    printf("  %d",sum[i][j]);
+                }    
+
+            
     return 0;
 }
